1ebcee7a89b918f7836225f9a81882e370e12bae
kaan demirel <kaan demirel@DESKTOP-HOEGLU8>
2024-02-09 06:07:12 +0300

    Add project files.

---
+++ WorkSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Vatandas
+{
+    public string Ad  { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+
+}
+
+
+
+
